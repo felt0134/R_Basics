a6b8d32d9f8f3b923fea754617631d3067970fa1
felt0134 <felton12392@gmail.com>
2024-02-02 16:16:33 -0700

    functions tutorial, about 1/2 done.

--- R/Start.R
+++ R/Start.R
@@ -1,9 +1,9 @@
 
-#make package
-usethis::create_package(getwd(),check_name = F) 
+#make this package
+#usethis::create_package(getwd(),check_name = F) 
 
 #create the tutorial
-usethis::use_tutorial("Week_4_Operators", "Week_4_Operators",open=interactive())
+usethis::use_tutorial("Week_4_Functions", "Week_4_Functions",open=interactive())
 
 #push to github
 devtools::install_github("felt0134/R_Basics")
