e4c7303d378288726e743f6b089e9e6b4751d9bf
felt0134 <felton12392@gmail.com>
2024-02-02 14:48:40 -0700

    small changes

--- R/Start.R
+++ R/Start.R
@@ -1,9 +1,9 @@
 
 #make package
-#usethis::create_package(getwd()) 
+usethis::create_package(getwd(),check_name = F) 
 
 #create the tutorial
-usethis::use_tutorial("Week_4_Operators", "Week_4_Operators")
+usethis::use_tutorial("Week_4_Operators", "Week_4_Operators",open=interactive())
 
 #push to github
 devtools::install_github("felt0134/R_Basics")
