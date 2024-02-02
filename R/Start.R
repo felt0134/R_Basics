34eceea94051eb472e8a44747449f74230e146f6
felt0134 <felton12392@gmail.com>
2024-02-02 14:30:46 -0700

    first push

---
+++ R/Start.R
@@ -0,0 +1,11 @@
+
+#make package
+#usethis::create_package(getwd()) 
+
+#create the tutorial
+usethis::use_tutorial("Week_4_Operators", "Week_4_Operators")
+
+#push to github
+devtools::install_github("felt0134/R_Basics")
+
+?install_github
