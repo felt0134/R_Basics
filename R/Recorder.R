50d9d0c6b1d9d9ddc9aa8a6213d93d4ac9ea4b97
felt0134 <felton12392@gmail.com>
2024-02-06 15:01:39 -0700

    try recorder function

--- R/Recorder.R
+++ R/Recorder.R
@@ -25,22 +25,3 @@ recorder <- function(tutorial_id, tutorial_version, user_id, event, data) {
     save(tutorial_data, file = recordFile)
   }
 }
-
-submit_ENSC311 <- function(tutorial_index = NULL) {
-  tut_files <- dir(tut_directory())
-  if(is.null(tutorial_index)) return(tut_files)
-  tutorial_index <- as.integer(tutorial_index)
-  if(tutorial_index < 1 || tutorial_index > length(tut_files)) stop("tutorial_index is out of range")
-  source_name <- file.path(tut_directory(), tut_files[tutorial_index])
-  dest_name <- file.path(getwd(), paste0("submit_", tut_files[tutorial_index]))
-  file.copy(source_name, dest_name, overwrite = T)
-}
-
-tut_directory <- function() {
-  file.path("~", "_311_Tutorial_")
-}
-
-tut_filename <- function(tutorial, user) {
-  paste0(basename(tutorial), "_311_", user)
-}
-
