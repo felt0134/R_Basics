f113ecc9ab90856c7ed4c93d15f7a2096890d326
felt0134 <felton12392@gmail.com>
2024-02-06 15:16:17 -0700

    added submit function

---
+++ R/Submit.R
@@ -0,0 +1,30 @@
+#' Create a homework submission file for ENSC311 tutorials
+#'
+#' Creates a homework submission file in your working directory.
+#'
+#' To use this function, first run the function without passing arguments.
+#' Examine the resulting list of files that can be created and determine the
+#' index of the file you want to create. and then run the function again,
+#' passing the index number of the file to be created.
+#'
+#' You can determine your working directory (where the file is created) with
+#' \code{\link{getwd}}.
+#'
+#' @return Either a vector of available homework submission files or TRUE if the
+#'   homework submission file was created in your working directory correctly.
+#'
+#' @param tutorial_index An index indicating which homework submission file to
+#'   retrieve.  If NULL, the vector of available homework submission files is
+#'   returned.
+#' @export
+#' 
+#'
+submit_ENSC311 <- function(tutorial_index = NULL) {
+  tut_files <- dir(tut_directory())
+  if(is.null(tutorial_index)) return(tut_files)
+  tutorial_index <- as.integer(tutorial_index)
+  if(tutorial_index < 1 || tutorial_index > length(tut_files)) stop("tutorial_index is out of range")
+  source_name <- file.path(tut_directory(), tut_files[tutorial_index])
+  dest_name <- file.path(getwd(), paste0("submit_", tut_files[tutorial_index]))
+  file.copy(source_name, dest_name, overwrite = T)
+}
